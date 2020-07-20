529eac9929b78325a180a84a780f1914bf91678c
turkuaz <berkayacademic@gmail.com>
2020-07-21 01:22:46 +0300

    User interface improvements

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import *
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('index.html')
+
+@app.route('/success', methods = ['POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['customFile']
+        f.save(f.filename)
+        return render_template('colorized.html', name = f.filename)
