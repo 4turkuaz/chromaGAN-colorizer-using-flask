d84b50d912a19ee32e5eecc729bcbe6ede1ba9b6
turkuaz <berkayacademic@gmail.com>
2020-07-21 16:54:48 +0300

    Finalizing the user interface

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from flask import *
 from werkzeug.utils import secure_filename
+from src import colorize
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static', static_folder = 'static')
 
 # Create a directory in a known location to save files to.
 uploads_dir = os.path.join(app.instance_path, 'uploads')
@@ -17,5 +18,5 @@ def upload_image():
     if request.method == 'POST':
         f = request.files['image']
         f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
-        print(uploads_dir)
-        return render_template('loading.html', name = f.filename)
+        output = colorize.colorize(uploads_dir)
+        return render_template('colorized.html', picture = output, original_picture = f.filename)
