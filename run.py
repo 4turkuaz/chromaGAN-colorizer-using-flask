0f451ae7535387471cf18c6e8a8649147be8b5a2
turkuaz <berkayacademic@gmail.com>
2020-07-21 13:51:17 +0300

    User interface improvements

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
+import os
 from flask import *
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+# Create a directory in a known location to save files to.
+uploads_dir = os.path.join(app.instance_path, 'uploads')
+os.makedirs(uploads_dir, exist_ok=True)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/success', methods = ['POST'])
-def upload():
+@app.route('/upload-image', methods = ['POST'])
+def upload_image():
     if request.method == 'POST':
-        f = request.files['customFile']
-        f.save(f.filename)
-        return render_template('colorized.html', name = f.filename)
+        f = request.files['image']
+        f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
+        print(uploads_dir)
+        return render_template('loading.html', name = f.filename)
